f5d9dd9f7e3e35f227906e7cfc48814f31fcef47
DevZakariaElkashef <dev.zakariaelkashef@gmail.com>
2024-12-28 11:43:28 +0200

    adding post type events

---
+++ web/app/plugins/events/includes/Admin/PostTypes.php
@@ -0,0 +1,57 @@
+<?php
+
+
+namespace Events\Admin;
+
+class PostTypes
+{
+    public function __construct()
+    {
+        add_action("init", [$this,"register_post_type"]);
+    }
+
+    public function register_post_type()
+    {
+        $labels = [
+            'name'                  => __('Events', 'events'),
+            'singular_name'         => __('Event', 'events'),
+            'menu_name'             => __('Events', 'events'),
+            'name_admin_bar'        => __('Event', 'events'),
+            'add_new'               => __('Add New', 'events'),
+            'add_new_item'          => __('Add New Event', 'events'),
+            'edit_item'             => __('Edit Event', 'events'),
+            'new_item'              => __('New Event', 'events'),
+            'view_item'             => __('View Event', 'events'),
+            'view_items'            => __('View Events', 'events'),
+            'search_items'          => __('Search Events', 'events'),
+            'not_found'             => __('No events found.', 'events'),
+            'not_found_in_trash'    => __('No events found in Trash.', 'events'),
+            'parent_item_colon'     => __('Parent Event:', 'events'),
+            'all_items'             => __('All Events', 'events'),
+            'archives'              => __('Event Archives', 'events'),
+            'attributes'            => __('Event Attributes', 'events'),
+            'insert_into_item'      => __('Insert into event', 'events'),
+            'uploaded_to_this_item' => __('Uploaded to this event', 'events'),
+            'featured_image'        => __('Featured Image', 'events'),
+            'set_featured_image'    => __('Set featured image', 'events'),
+            'remove_featured_image' => __('Remove featured image', 'events'),
+            'use_featured_image'    => __('Use as featured image', 'events'),
+            'filter_items_list'     => __('Filter events list', 'events'),
+            'items_list_navigation' => __('Events list navigation', 'events'),
+            'items_list'            => __('Events list', 'events'),
+        ];
+        
+        $args = [
+            'label'               => __('Events', 'events'),
+            'labels'              => $labels,
+            'public'              => true,
+            'has_archive'         => true,
+            'rewrite'             => ['slug' => 'events'],
+            'supports'            => ['title', 'editor'], 
+            'taxonomies'          => ['event_category', 'event_tag'],
+            'show_in_menu'        => true,
+        ];
+        register_post_type('event', $args);
+        
+    }
+}
---
+++ web/app/plugins/events/includes/Admin/Taxonomies.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Events\Admin;
+
+class Taxonomies
+{
+    public function __construct()
+    {
+        add_action('init', [$this, 'register_categories_taxonomies']);
+        add_action('init', [$this, 'register_tags_taxonomies']);
+    }
+
+
+    public function register_categories_taxonomies()
+    {
+        // Event Category
+        $labels = [
+            'name'              => __('Event Categories', 'events'),
+            'singular_name'     => __('Event Category', 'events'),
+            'search_items'      => __('Search Event Categories', 'events'),
+            'all_items'         => __('All Event Categories', 'events'),
+            'edit_item'         => __('Edit Event Category', 'events'),
+            'update_item'       => __('Update Event Category', 'events'),
+            'add_new_item'      => __('Add New Event Category', 'events'),
+            'new_item_name'     => __('New Event Category Name', 'events'),
+            'menu_name'         => __('Event Categories', 'events'),
+        ];
+
+        register_taxonomy('event_category', 'event', [
+            'hierarchical'      => true,
+            'labels'            => $labels,
+            'show_ui'           => true,
+            'show_admin_column' => true,
+            'query_var'         => true,
+            'rewrite'           => ['slug' => 'event-category'],
+        ]);
+    }
+
+    public function register_tags_taxonomies()
+    {
+        // Event Tags
+        $labels = [
+            'name'              => __('Event Tags', 'events'),
+            'singular_name'     => __('Event Tag', 'events'),
+            'search_items'      => __('Search Event Tags', 'events'),
+            'popular_items'     => __('Popular Event Tags', 'events'),
+            'all_items'         => __('All Event Tags', 'events'),
+            'edit_item'         => __('Edit Event Tag', 'events'),
+            'update_item'       => __('Update Event Tag', 'events'),
+            'add_new_item'      => __('Add New Event Tag', 'events'),
+            'new_item_name'     => __('New Event Tag Name', 'events'),
+            'separate_items_with_commas' => __('Separate event tags with commas', 'events'),
+            'add_or_remove_items' => __('Add or remove event tags', 'events'),
+            'choose_from_most_used' => __('Choose from the most used event tags', 'events'),
+            'menu_name'         => __('Event Tags', 'events'),
+        ];
+
+        register_taxonomy('event_tag', 'event', [
+            'hierarchical'      => false,
+            'labels'            => $labels,
+            'show_ui'           => true,
+            'show_admin_column' => true,
+            'query_var'         => true,
+            'rewrite'           => ['slug' => 'event-tag'],
+        ]);
+    }
+}
